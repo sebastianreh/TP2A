6161679a39642b2ccc858a0556631e750d9dea66
Pablo Fernandez <pablofernandez@MacBook-Pro-de-Pablo.local>
2020-04-29 22:05:03 -0300

    chalk

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const validator = require('validator');
 
 console.log(validator.isEmail('pablof@tecnoshare.com'));
 
+const chalk = require('chalk');
+
+console.log(chalk.blue.bgGray('Hello world'));
