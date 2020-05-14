511a4199963eeec0d3fc9cc2a25a0ae2eb730026
Sebastian Reh <sebastian.reh@rappi.com>
2020-05-13 22:43:12 -0300

    Resolución del Desafio 2

--- mongo/coneccion.js
+++ mongo/coneccion.js
@@ -1,37 +1,91 @@
 const mongoclient = require('mongodb').MongoClient;
-const chalk = require('chalk');
+const mongoConnectionString = "mongodb+srv://admin:betp2@cluster0-zdy6w.mongodb.net/test?retryWrites=true&w=majority";
 
-const uri = "mongodb+srv://admin:betp2@cluster0-zdy6w.mongodb.net/test?retryWrites=true&w=majority";
-const client = new mongoclient(uri, {useNewUrlParser:true, useUnifiedTopology:true});
+function createMongoClient() {
+    return new mongoclient(mongoConnectionString, {useNewUrlParser: true, useUnifiedTopology: true});
+}
 
-client.connect((err, result) =>{
-    if(!err){
-        console.log(chalk.blue('Cliente conectado'));
-        let collection = result.db("sample_betp2").collection("inventors");
-        collection.find().limit(20).toArray((err, result) => {
-            console.log(result);
-            // insertar un nuevo inventor
-            const nuevoInventor = {
-                first: "Pedro",
-                last: "Perez",
-                year: 1987
-            }
+//INSERT INVENTOR
+async function insertInventor(newInventor) {
+    try {
+        let cli = await createMongoClient().connect();
+        let inventorsCollection = cli.db("sample_betp2").collection("inventors");
 
-            collection.insertOne(nuevoInventor, (error, result) => {
-                if(!error){
-                    console.log(chalk.yellow("Inventor insertado correctamente"));
-                    //nuevoInventor.year = 2000;
-                    // update de un campo
-                    collection.updateOne({last: "Perez"}, {$set: {year: 2000}} , (err, result) => {
-                        console.log(chalk.yellow("Inventor actualizado correctamente"));
-                        collection.deleteOne({last: "Perez"}, (err, result) => {
-                            console.log(chalk.yellow("Inventor eliminado correctamente"));
-                        });
-                    });
-                }
-            });
-        });
-    } else {
-        console.log(chalk.red(err));
+        await inventorsCollection.insertOne(newInventor);
+        console.log(`Inventor ${newInventor.firstName} ${newInventor.lastName} successfully added to the data base`);
+
+        await cli.close();
+    } catch (error) {
+        console.log(`Error inserting inventor: ${error.stack}`)
     }
-});
+}
+
+//SEARCH INVENTOR
+async function searchInventor(inventorToFind) {
+    try {
+        let cli = await createMongoClient().connect();
+        let collection = cli.db("sample_betp2").collection("inventors");
+
+        let requiredInventor = await collection.find({lastName: inventorToFind.lastName}).toArray();
+        if (requiredInventor) {
+            console.log(`Inventor found: ${inventorToFind.firstName} ${inventorToFind.lastName}`);
+        } else {
+            console.log(`Inventor ${inventorToFind.firstName} ${inventorToFind.lastName} was not found"`);
+        }
+
+        await cli.close();
+    } catch (error) {
+        console.log(`Error searching for inventor ${error}`)
+    }
+}
+
+//MODIFY INVENTOR
+async function modifyInventor(inventorToModify, inventorModified) {
+    try {
+        let cli = await createMongoClient().connect();
+        let collection = cli.db("sample_betp2").collection("inventors");
+
+        await collection.updateOne({lastName: inventorToModify.lastName}, {$set: {lastName: inventorModified.lastName}});
+        console.log(`inventor ${inventorToModify.lastName} successfully updated`);
+
+        await cli.close();
+    } catch (error) {
+        console.log(`Error modifying inventor ${error}`)
+    }
+}
+
+//DELETE INVENTOR
+async function deleteInventor(inventorToDelete) {
+    try {
+        let cli = await createMongoClient().connect();
+        let collection = cli.db("sample_betp2").collection("inventors");
+
+        await collection.deleteOne({lastName: inventorToDelete.lastName});
+        console.log(`inventor ${inventorToDelete.lastName} successfully deleted`);
+
+        return cli.close().then();
+    } catch (error) {
+        return console.log(error).then();
+    }
+}
+
+async function RunMongoTest(Inventor1, Inventor2) {
+    await insertInventor(Inventor1);
+    await searchInventor(Inventor1);
+    await modifyInventor(Inventor1, Inventor2);
+    await deleteInventor(Inventor2);
+}
+
+const InventorMock = {
+    firstName: "Sebastian",
+    lastName: "Reh",
+    year: 1990
+};
+
+const InventorMockToModify = {
+    firstName: "Ricardo",
+    lastName: "Reh",
+    year: 1990
+};
+
+RunMongoTest(InventorMock, InventorMockToModify);
